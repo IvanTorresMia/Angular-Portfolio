61d8db4121dfa2a5ff2fb235b04ac2a4a8002c07
Ivan Torres <ivantorresmia27@gmail.com>
2021-10-18 18:03:22 -0700

    header boilerplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HeaderComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
